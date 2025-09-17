e7d3b85a5c5bdeab102b36926708c2d5e061274c
Nayana <nithinrnithinr440@gmail.com>
2025-09-17 13:21:58 +0530

    FIrst Commit

---
+++ SeleniumProject/src/scripting4/Testscript.java
@@ -0,0 +1,54 @@
+package scripting4;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Map;
+
+import static scripting4.ReusableMethod.*;
+
+public class Testscript {
+        public static void main(String[] args) {
+            TS_LoginLogout();
+            TS_createAndDeleteUser();
+
+        }
+
+        public static void TS_LoginLogout() {
+            WebDriver oBrowser = null;
+            Map<String, String> data = null;
+            try {
+                data = getExcelData("TestData", "testData","TC_ID_001");
+                oBrowser = launchBrowser(data.get("browserName"));
+                boolean blnRes = navigateURL(oBrowser, data.get("url"));
+                blnRes = loginToApplication(oBrowser, data.get("userName") , data.get("password"));
+                blnRes = logoutFromActiTime(oBrowser);
+
+            } catch (Exception e) {
+                System.out.println("Exception in 'TS_LoginLogout' test script");
+            } finally {
+                oBrowser.close();
+                oBrowser = null;
+            }
+        }
+        public static void TS_createAndDeleteUser() {
+            WebDriver oBrowser = null;
+            Map<String, String> data = null;
+            try {
+                data = getExcelData("TestData","testData","TC_ID_002");
+                oBrowser = launchBrowser(data.get("browserName"));
+                boolean blnRes = navigateURL(oBrowser, data.get("url"));
+                blnRes = loginToApplication(oBrowser, data.get("userName") , data.get("password"));
+                String userName = createUser(oBrowser, data);
+                blnRes = deleteUser(oBrowser, userName);
+                blnRes = logoutFromActiTime(oBrowser);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                oBrowser.close();
+                oBrowser = null;
+            }
+        }
+    }
+
+
